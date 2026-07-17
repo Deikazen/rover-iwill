e6855443831c3132369f492239a211c94bfe24b0
Satria <mfsmfsmfs35@gmail.com>
2026-07-17 19:43:10 +0700

    add route

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from app.api.router import router
+import uvicorn
 
 app = FastAPI()
 
@@ -11,3 +13,9 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+
+app.include_router(router, prefix='/api')
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
